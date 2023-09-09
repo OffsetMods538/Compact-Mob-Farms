230e3d7da1a96c4479141637cfc3b6e6379bb1d2
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-09-09 12:42:51 +0300

    Correct hitbox for compact mod farm

--- src/main/java/top/offsetmonkey538/compactmobfarms/block/CompactMobFarmBlock.java
+++ src/main/java/top/offsetmonkey538/compactmobfarms/block/CompactMobFarmBlock.java
@@ -3,16 +3,29 @@ package top.offsetmonkey538.compactmobfarms.block;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import top.offsetmonkey538.compactmobfarms.block.entity.CompactMobFarmBlockEntity;
 import top.offsetmonkey538.compactmobfarms.block.entity.ModBlockEntityTypes;
 
 public class CompactMobFarmBlock extends BlockWithEntity {
+    public static final VoxelShape SHAPE = VoxelShapes.union(
+            VoxelShapes.cuboid(0.05625f, 0.0625f, 0.05625f, 0.94375f, 0.9375f, 0.94375f),
+
+            VoxelShapes.cuboid(0f,     0f, 0f,     0.125f,1f, 0.125f),
+            VoxelShapes.cuboid(0.875f, 0f, 0f,     1f,    1f, 0.125f),
+            VoxelShapes.cuboid(0.875f, 0f, 0.875f, 1f,    1f, 1f),
+            VoxelShapes.cuboid(0f,     0f, 0.875f, 0.125f,1f, 1f)
+    );
+
     public CompactMobFarmBlock(Settings settings) {
         super(settings);
     }
@@ -26,6 +39,12 @@ public class CompactMobFarmBlock extends BlockWithEntity {
     @Override
     public BlockRenderType getRenderType(BlockState state) {
         return BlockRenderType.MODEL;
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
     }
 
     @Nullable
