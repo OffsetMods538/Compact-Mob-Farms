52844181c7bc43244f6eb2cbea66c6151f2bc08c
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-09-09 13:02:03 +0300

    Store the sample taker item inside the block entity

--- src/main/java/top/offsetmonkey538/compactmobfarms/block/entity/CompactMobFarmBlockEntity.java
+++ src/main/java/top/offsetmonkey538/compactmobfarms/block/entity/CompactMobFarmBlockEntity.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
@@ -15,9 +21,32 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import top.offsetmonkey538.compactmobfarms.inventory.CompactMobFarmInventory;
+import top.offsetmonkey538.compactmobfarms.item.SampleTakerItem;
 
 public class CompactMobFarmBlockEntity extends BlockEntity implements CompactMobFarmInventory {
-    List<ItemStack> dropInventory = new ArrayList<>(1);
+    final List<ItemStack> dropInventory = new ArrayList<>(1);
+    private final SimpleInventory sampleTaker = new SimpleInventory(1) {
+        @Override
+        public int getMaxCountPerStack() {
+            return 1;
+        }
+
+        @Override
+        public boolean canInsert(ItemStack stack) {
+            return SampleTakerItem.getSamplesCollected(stack).size() >= 10 && super.canInsert(stack);
+        }
+
+        @Override
+        public boolean isValid(int slot, ItemStack stack) {
+            return canInsert(stack);
+        }
+
+        @Override
+        public boolean canTransferTo(Inventory hopperInventory, int slot, ItemStack stack) {
+            return false;
+        }
+    };
+    int timer = 0;
 
     public CompactMobFarmBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntityTypes.COMPACT_MOB_FARM, pos, state);
@@ -32,6 +61,7 @@ public class CompactMobFarmBlockEntity extends BlockEntity implements CompactMob
         NbtList dropInventoryNbt = new NbtList();
         this.dropInventory.forEach(item -> dropInventoryNbt.add(item.writeNbt(new NbtCompound())));
         nbt.put("DropInventory", dropInventoryNbt);
+        nbt.put("SampleTaker", sampleTaker.toNbtList());
 
         super.writeNbt(nbt);
     }
@@ -42,6 +72,7 @@ public class CompactMobFarmBlockEntity extends BlockEntity implements CompactMob
 
         nbt.getList("DropInventory", NbtElement.COMPOUND_TYPE)
                 .forEach(itemNbt -> this.dropInventory.add(ItemStack.fromNbt((NbtCompound) itemNbt)));
+        sampleTaker.readNbtList(nbt.getList("SampleTaker", NbtElement.COMPOUND_TYPE));
     }
 
     @Override
