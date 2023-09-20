fede9d78b76790db8145367d942bd46cb30c3602
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-09-20 16:34:56 +0300

    Entity rendering in the gui

--- src/client/java/top/offsetmonkey538/compactmobfarms/client/gui/screen/ingame/CompactMobFarmScreen.java
+++ src/client/java/top/offsetmonkey538/compactmobfarms/client/gui/screen/ingame/CompactMobFarmScreen.java
@@ -1,20 +1,27 @@
 package top.offsetmonkey538.compactmobfarms.client.gui.screen.ingame;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
-import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.gui.widget.CyclingButtonWidget;
+import net.minecraft.client.render.DiffuseLighting;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import org.joml.Quaternionf;
+import org.joml.Vector3f;
 import top.offsetmonkey538.compactmobfarms.screen.CompactMobFarmScreenHandler;
 
 import static top.offsetmonkey538.compactmobfarms.CompactMobFarms.*;
 
 public class CompactMobFarmScreen extends HandledScreen<CompactMobFarmScreenHandler> {
+    private static final Quaternionf ENTITY_ROTATION = new Quaternionf().rotationXYZ(0, 0, (float) Math.PI);
+    private static final Vector3f FRONT_UP_LIGHT = new Vector3f(0, -1, 0.5f).normalize();
     private static final Identifier TEXTURE = id("textures/gui/container/compact_mob_farm.png");
     private LivingEntity entity;
 
@@ -56,12 +63,10 @@ public class CompactMobFarmScreen extends HandledScreen<CompactMobFarmScreenHand
 
         if (entity == null) return;
 
-        int entityX = x + 118;
-        int entityY = y + 20;
-        // TODO: I don't like that it's following the cursor.
-        //  Create a new method and make it render the entity like uhh isometrically or something similar
-        //  Also it's not in the correct place because why would it be ._.
-        InventoryScreen.drawEntity(context, entityX, entityY, 46, entityX - mouseX, entityY - mouseY, entity);
+        int entityX = x + 140;
+        int entityY = y + 63;
+
+        renderEntity(context, entity, entityX, entityY, 42, ENTITY_ROTATION);
     }
 
     @Override
@@ -71,6 +76,25 @@ public class CompactMobFarmScreen extends HandledScreen<CompactMobFarmScreenHand
         super.render(context, mouseX, mouseY, delta);
 
         drawMouseoverTooltip(context, mouseX, mouseY);
+    }
+
+    public static void renderEntity(DrawContext context, LivingEntity entity, int x, int y, int boxSize, Quaternionf rotation) {
+        float entityScale = boxSize / Math.max(entity.getWidth(), entity.getHeight());
+
+        context.getMatrices().push();
+        context.getMatrices().translate(x, y, 50);
+        context.getMatrices().multiply(rotation);
+        context.getMatrices().scale(entityScale, entityScale, entityScale);
+        RenderSystem.setShaderLights(FRONT_UP_LIGHT, FRONT_UP_LIGHT);
+
+        EntityRenderDispatcher entityRenderDispatcher = MinecraftClient.getInstance().getEntityRenderDispatcher();
+        entityRenderDispatcher.setRenderShadows(false);
+        entityRenderDispatcher.render(entity, 0.0, 0.0, 0.0, 0.0f, 0, context.getMatrices(), context.getVertexConsumers(), 0xF000F0);
+        entityRenderDispatcher.setRenderShadows(true);
+
+        context.draw();
+        context.getMatrices().pop();
+        DiffuseLighting.enableGuiDepthLighting();
     }
 
     public void setEntity(EntityType<?> entityType) {
