6dbc623741532a8a174b709588242cade0ed029a
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-09-24 12:53:30 +0300

    Add recipe for compact mob farm

--- src/main/java/top/offsetmonkey538/compactmobfarms/datagen/ModRecipeProvider.java
+++ src/main/java/top/offsetmonkey538/compactmobfarms/datagen/ModRecipeProvider.java
@@ -5,9 +5,11 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.advancement.criterion.InventoryChangedCriterion;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.tag.ItemTags;
 import top.offsetmonkey538.compactmobfarms.item.ModItems;
 
 public class ModRecipeProvider extends FabricRecipeProvider {
@@ -23,6 +25,17 @@ public class ModRecipeProvider extends FabricRecipeProvider {
                 .input(Items.GOLD_NUGGET)
                 .input(Items.IRON_BARS)
                 .criterion("has_spirit_bottle_item", InventoryChangedCriterion.Conditions.items(ModItems.SPIRIT_BOTTLE))
+                .offerTo(exporter);
+
+        ShapedRecipeJsonBuilder
+                .create(RecipeCategory.MISC, ModItems.COMPACT_MOB_FARM)
+                .pattern("OSO")
+                .pattern("SsS")
+                .pattern("OSO")
+                .input('O', Items.OBSIDIAN)
+                .input('S', ModItems.SPAWNER_SHARD)
+                .input('s', ItemTags.STONE_BRICKS)
+                .criterion("has_spawner_shard_item", InventoryChangedCriterion.Conditions.items(ModItems.SPAWNER_SHARD))
                 .offerTo(exporter);
     }
 }
